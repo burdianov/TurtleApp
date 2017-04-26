4e523f4b11b3c1ca93e37a7f96cd1c72a6c0e493
Lilian Burdianov <lilian.burdianov@gmail.com>
2017-04-26 23:00:58 +0400

    Initial commit

---
+++ app/src/main/java/com/crackncrunch/turtleapp/TurtleActivity.java
@@ -0,0 +1,14 @@
+package com.crackncrunch.turtleapp;
+
+import android.os.Bundle;
+
+import stanford.androidlib.SimpleActivity;
+
+public class TurtleActivity extends SimpleActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turtle);
+    }
+}
